de33df20a83e4f4b4d26a785de9c1d446b3feae0
fuyi <1312170600@qq.com>
2020-09-22 23:48:31 +0800

    完成全部连接池demo小练习

---
+++ druid-multi/src/main/java/com/ryan/druidmulti/config/DataSourceConfig.java
@@ -0,0 +1,34 @@
+package com.ryan.druidmulti.config;
+
+import com.alibaba.druid.spring.boot.autoconfigure.DruidDataSourceBuilder;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
+
+import javax.sql.DataSource;
+
+
+@Configuration
+public class DataSourceConfig {
+
+    /**
+     * 创建 scheduleer 数据源
+     */
+    @Primary
+    @Bean(name = "ordersDataSource")
+    @ConfigurationProperties(prefix = "spring.datasource.orders") // 读取 spring.datasource.orders 配置到 HikariDataSource 对象
+    public DataSource ordersDataSource() {
+        return DruidDataSourceBuilder.create().build();
+    }
+
+    /**
+     * 创建 quartz 数据源
+     */
+    @Bean(name = "usersDataSource")
+    @ConfigurationProperties(prefix = "spring.datasource.users")
+    public DataSource usersDataSource() {
+        return DruidDataSourceBuilder.create().build();
+    }
+
+}
